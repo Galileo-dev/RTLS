44b4b4938dbf28b6c5065c18cd4237c2c56abb49
Galileo <Galileo-dev@users.noreply.github.com>
2021-03-07 20:53:35 +0000

    added checklist

---
+++ src/components/dashboard/ProgressBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function ProgressBarList() {
+  return (
+    <div>
+      <ProgressBar />
+    </div>
+  );
+}
+
+function ProgressBar() {
+  return <div></div>;
+}
+
+export { ProgressBar, ProgressBarList };
