b74083c85b88c2f65d17d3be9f0eaf5f990f8acc
Galileo <Galileo-dev@users.noreply.github.com>
2021-03-07 22:27:38 +0000

    Added Smoother Counting

--- app.js
+++ app.js
@@ -3,13 +3,22 @@ const io = require("socket.io")();
 var sampleJson = require("./TheJsonStructure.json");
 var simple = 1;
 
+async function main() {
+  while (true) {
+    simple += 1;
+    if (simple >= 1000) {
+      simple = 0;
+    }
+    await timer(10);
+  }
+}
+main();
 io.on("connection", (client) => {
   client.on("subscribeToTelemetry", (interval) => {
     console.log("client is subscribing to telem with interval ", interval);
     setInterval(() => {
       //console.log(sampleJson);
       client.emit("telem", simple);
-      simple += 1;
     }, interval);
   });
 });
@@ -17,3 +26,7 @@ io.on("connection", (client) => {
 const port = 8000;
 io.listen(port);
 console.log("listening on port ", port);
+
+function timer(ms) {
+  return new Promise((res) => setTimeout(res, ms));
+}
